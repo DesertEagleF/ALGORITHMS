9622fb4a7c1d11f2daa35e97e773a95a446ddfb3
luxaobohit <luxaobohit@users.noreply.github.com>
2014-10-21 11:01:32 +0800

    Update and rename 2-2-Selection_sort.cpp to 2-2-SELECTION-SORTt.cpp

--- chapter02/2-2-SELECTION-SORTt.cpp
+++ chapter02/2-2-SELECTION-SORTt.cpp
@@ -1,16 +1,16 @@
 //selection sort
-void selection_sort(int arr[], int len)
+void Selection_Sort(int arr[], int len)
 {
-    if (arr = NULL || len < 1)
+    if (arr == NULL || len < 1)
         return;
     int i , j;
-    for (i = 0; i! = len-1; ++i)
+    for (i = 0; i!= len-1; ++i)
     {
         for (j = i+1; j!=len; ++j)
         {
             if ( arr[j] < arr[i])
             {
-                int tmp = =arr[j];
+                int tmp = arr[j];
                 arr[j] = arr[i];
                 arr[i] = tmp;
             }
