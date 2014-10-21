d0de4143720eb08ec91d8659c274773b05c613cf
luxaobohit <luxaobohit@users.noreply.github.com>
2014-10-21 10:58:06 +0800

    Create 1-2 INSERTION-SORT.cpp

---
+++ chapter02/1-2 INSERTION-SORT.cpp
@@ -0,0 +1,16 @@
+//INSERTION-SORT by descending/ascending order
+
+void Insertion_Sort_desc( int arr[], int n)
+{
+    int i, j;
+    for (i = 1; i!= n; ++i)
+    {
+        for (j = i; j > 0 && arr[j] > arr[j-1]; --j)  //descending order
+      //for (j = i; j > 0 && arr[j] < arr[j-1]; --j)  //ascending order
+        {
+            int tmp = arr[j];
+            arr[j] = arr[j-1];
+            arr[j-1] = tmp;
+        }
+    }
+}
