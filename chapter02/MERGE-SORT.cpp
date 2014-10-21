27073ffa695ea37a3ae78e01343367adb6c3c52c
luxaobohit <luxaobohit@users.noreply.github.com>
2014-10-21 11:02:29 +0800

    Update and rename MergeSort.cpp to MERGE-SORT.cpp

--- chapter02/MERGE-SORT.cpp
+++ chapter02/MERGE-SORT.cpp
@@ -1,13 +1,13 @@
 //Merge sort based on Merge Method
 
-void Merge_sort(int arr[], int begin, int end)
+void Merge_Sort(int arr[], int begin, int end)
 {
     if(begin < end) //condition to break the loop
     {    
         int mid = (begin + end)/2;
     
-        Merge_sort(arr, begin, mid);
-        Merge_sort(arr, mid+1, end);
+        Merge_Sort(arr, begin, mid);
+        Merge_Sort(arr, mid+1, end);
         Merge(arr, begin, mid, end);
     }
 }
